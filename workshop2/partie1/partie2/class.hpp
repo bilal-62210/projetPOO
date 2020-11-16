b735d30e705124724afba83e835706f8f5f63b4b
bilal-62210 <bilal.benelkadi@viacesi.fr>
2020-11-16 15:09:57 +0100

    ws

---
+++ workshop2/partie1/partie2/class.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+class A
+{
+    protected:
+        int x;
+    public:
+         virtual void m(void);
+        
+};
+
+class B : public A
+{
+    protected:
+        int z;
+    public:
+        void m(void);
+};
+
+class stockage : public A, public B
+{
+        protected:
+
+        public: 
+            void ajouter(void);
+
+}
